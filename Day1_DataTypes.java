f8a53c43a2cb81f1fe7b61cf2061d668190749fb
prasadaswale1996 <prasadaswale1996@gmail.com>
2022-06-08 23:17:46 +0530

    creating table of given number

---
+++ Day1_DataTypes.java
@@ -0,0 +1,47 @@
+package hackerRank30DaysChalange;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Day1_DataTypes {
+	
+    public static void main(String[] args) {
+        int i = 4;
+        double d = 4.0;
+        String s = "HackerRank ";
+		
+        System.out.println(i);
+        System.out.println(d);
+        System.out.println(s);
+        
+        Scanner scan = new Scanner(System.in);
+        	
+        int i1= scan.nextInt();
+    	double d1= scan.nextDouble();
+    	scan.nextLine();
+        	String s1 = scan.nextLine();
+        	
+        	
+        	System.out.println(s+s1);
+        	System.out.println(i1+i);
+            System.out.println(d1+d);
+           
+        /* Declare second integer, double, and String variables. */
+
+        /* Read and save an integer, double, and String to your variables.*/
+        // Note: If you have trouble reading the entire String, please go back and review the Tutorial closely.
+
+        /* Print the sum of both integer variables on a new line. */
+
+        /* Print the sum of the double variables on a new line. */
+		
+        /* Concatenate and print the String variables on a new line; 
+        	the 's' variable above should be printed first. */
+
+        scan.close();
+        
+    }
+}
---
+++ Loops.java
@@ -0,0 +1,19 @@
+package hackerRank30DaysChalange;
+
+import java.util.Scanner;
+
+public class Loops {
+
+		// table creation of given no..
+	public static void main(String[] args) {
+		//int n = 1;
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();	// give any no in input
+				
+		for (int i=1; i<=10; i++) {
+			System.out.println(n+"*"+i+"="+n*i);
+		}
+		
+		
+	}	
+}
